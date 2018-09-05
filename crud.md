af76f774f720b8633d554f012b683c48fd98ec17
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2018-09-05 23:49:24 +0200

    added the urls

---
+++ crud.md
@@ -0,0 +1,4 @@
+CREATE - POST - Make new
+READ - GET - List/search
+UPDATE - PUT/PATCH - Edit
+DELETE - DELETE - Delete
--- posts/admin.py
+++ posts/admin.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Post
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "timestamp", "updated"]
+    list_display_links = ["updated"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["title", "content"]
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post)
