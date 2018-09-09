e017b31328ba54206235e590898a1361aadec76f
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2018-09-09 16:56:38 +0200

    added links

--- Postel/settings.py
+++ Postel/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '(qo#%!p2g4*2hsb=&@95a+x_z_nju8ll0#%n4scg+kbp=ptbt#'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -40,7 +38,7 @@ INSTALLED_APPS = [
     'posts',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Postel.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -114,7 +109,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 # Create your views here.
@@ -11,12 +12,32 @@ def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 
-def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+def post_detail(request, id):
+    # instance = Post.objects.get(id=1)
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "title": instance.title,
+        "instance": instance
+    }
+
+    # return HttpResponse("<h1>Detail</h1>")
+
+    return render(request, "post_detail.html", context)
 
 
 def post_list(request):
-    return render(request, "index.html", {})
+    queryset = Post.objects.all()
+
+    if request.user.is_authenticated():
+        context = {
+            "object_list": queryset,
+            "title": "You're in..."
+        }
+    else:
+        context = {
+            "title": "Need to login"
+        }
+    return render(request, "index.html", context)
     # return HttpResponse("<h1>Welcome</h1>")
 
 
